b0f17748bd47b174a4f5dc312c9898258b16ec79
greysakura <greysakura@qq.com>
2016-10-24 18:53:29 +0800

    H5PY

--- OtherTest/USEH5PY.py
+++ OtherTest/USEH5PY.py
@@ -2,11 +2,13 @@ import numpy as np
 import h5py
 from scipy.io import loadmat
 
-f = h5py.File('/Tokei_Mac/Work_for_work/project-mm2016/data/NUS-WIDE/nuswide_mm2016.mat')
+f = h5py.File('../data/nuswide_mm2016.mat')
 
 print type(f)
 print f
 
-ff = loadmat('/Tokei_Mac/Work_for_work/project-mm2016/data/NUS-WIDE/nuswide_mm2016.mat')
+# ff = loadmat('../data/nuswide_mm2016.mat')
+#
+# print ff
 
-print ff
+f.keys()
