dd6153724243235b02585d187ee40ac15635fc41
greysakura <greysakura@qq.com>
2016-10-27 15:07:15 +0800

    Flickr30k

--- src/Run.py
+++ src/Run.py
@@ -1,22 +1,25 @@
 import numpy as np
-import os
 import random
 import h5py
 from numpy import genfromtxt
 from fasttag_cross import fasttag_cross
 from evaluatePR import resultPR, evaluatePR
+from scipy.io import loadmat
 
 if __name__ == "__main__":
     numval = 1000
     topK = 5
 
-    type_file_input = 'mat'
+    # type_file_input = 'mat'
+    # dataset_to_test = 'nuswide'
+    dataset_to_test = 'flickr30k'
+
+    MatFile = None
 
     # when using mat files...
-    if 'mat' == type_file_input:
-
+    # if 'mat' == type_file_input:
+    if 'nuswide' == dataset_to_test:
         MatFile = h5py.File('../data/nuswide_mm2016.mat', 'r')
-
         trainIdx = MatFile['trainIdx'][:] - 1
         trainIdx = trainIdx.astype(int).flatten().tolist()
 
@@ -26,29 +29,80 @@ if __name__ == "__main__":
         tTe = MatFile['T_te'][:]
         yTr = MatFile['L_tr'][:][:, trainIdx]
         yTe = MatFile['L_te'][:]
+        nTr = xTr.shape[1]
+        nTe = xTe.shape[1]
 
+        print xTr.shape
+        print xTe.shape
+        print tTr.shape
+        print tTe.shape
+        print yTr.shape
+        print yTe.shape
 
-    # when using txt files...
-    if 'txt' == type_file_input:
-        tmpDataset = '../NUS_WIDE'
+        raw_input()
 
-        xTr = genfromtxt(os.path.join(tmpDataset, 'xTr.txt'), delimiter=' ')
-        xTe = genfromtxt(os.path.join(tmpDataset, 'xTe.txt'), delimiter=' ')
-        yTr = genfromtxt(os.path.join(tmpDataset, 'yTr.txt'), delimiter=' ')
-        yTe = genfromtxt(os.path.join(tmpDataset, 'yTe.txt'), delimiter=' ')
-        tTr = genfromtxt(os.path.join(tmpDataset, 'tTr.txt'), delimiter=' ')
-        tTe = genfromtxt(os.path.join(tmpDataset, 'tTe.txt'), delimiter=' ')
+        valIdx = random.sample(range(nTr), numval)
+        valIdx.sort()
 
-    nTr = xTr.shape[1]
-    nTe = xTe.shape[1]
+    elif 'flickr30k' == dataset_to_test:
+        numtrain = 5000
+        # MatFile = h5py.File('../data/flickr30k_index_mm2016.mat', 'r')
 
-    # TrList = range(nTr)
-    # random.shuffle(TrList)
-    # valIdx = TrList[:numval]
+        SX = h5py.File('../data/Flickr30k/flickr30k_features.mat')['SX'][:]
+        SY = h5py.File('../data/Flickr30k/flickr30k_features.mat')['SY'][:]
 
-    valIdx = random.sample(range(nTr), numval)
-    valIdx.sort()
-    print valIdx
+        trainIdx = h5py.File('../data/Flickr30k/index_training_testing/ind_train.mat')['ind_train'][:] - 1
+        trainIdx = trainIdx.astype(int).flatten().tolist()
+        print min(trainIdx)
+
+        tmpfile = loadmat('../data/Flickr30k/index_training_testing/3k_ind.mat')
+
+        # valIdx = tmpfile['devind'][:] - 1
+        # valIdx = valIdx.astype(int).flatten().tolist()
+
+        testind = tmpfile['testind'][:] - 1
+        testind = testind.astype(int).flatten().tolist()
+
+        tmpfile = loadmat('../data/Flickr30k/flickr30k_wordMatrix.mat')
+        wordMatrix = loadmat('../data/Flickr30k/flickr30k_wordMatrix.mat')['wordMatrix'][:]
+
+        xTr = SX[:, trainIdx[0:numtrain]]
+        xTe = SX[:, testind]
+        tTr = SY[:, trainIdx[0:numtrain]]
+        tTe = SY[:, testind]
+
+        yTr = wordMatrix[trainIdx[0:numtrain], :].transpose().astype(float)
+        yTe = wordMatrix[testind, :].transpose().astype(float)
+
+        nTr = xTr.shape[1]
+        nTe = xTe.shape[1]
+
+        print 'Calc: ', nTr, nTe
+
+        valIdx = random.sample(range(nTr), numval)
+        valIdx.sort()
+
+        print xTr.shape
+        print xTe.shape
+        print tTr.shape
+        print tTe.shape
+        print yTr.shape
+        print yTe.shape
+
+    else:
+        print 'Invalid dataset!'
+        exit()
+
+    # # when using txt files...
+    # if 'txt' == type_file_input:
+    #     tmpDataset = '../NUS_WIDE'
+    #
+    #     xTr = genfromtxt(os.path.join(tmpDataset, 'xTr.txt'), delimiter=' ')
+    #     xTe = genfromtxt(os.path.join(tmpDataset, 'xTe.txt'), delimiter=' ')
+    #     yTr = genfromtxt(os.path.join(tmpDataset, 'yTr.txt'), delimiter=' ')
+    #     yTe = genfromtxt(os.path.join(tmpDataset, 'yTe.txt'), delimiter=' ')
+    #     tTr = genfromtxt(os.path.join(tmpDataset, 'tTr.txt'), delimiter=' ')
+    #     tTe = genfromtxt(os.path.join(tmpDataset, 'tTe.txt'), delimiter=' ')
 
     xTr = np.concatenate((xTr, np.ones((1, xTr.shape[1]), int)), axis=0)
     xTe = np.concatenate((xTe, np.ones((1, xTe.shape[1]), int)), axis=0)
